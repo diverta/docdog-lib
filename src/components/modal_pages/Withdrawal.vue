1030d57736a631a13bb3892b7f40790e3434a806
mimori <mimori@diverta.co.jp>
2022-02-18 12:46:16 +0900

    Add: Withdrawal modal

---
+++ src/components/modal_pages/Withdrawal.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- ToDo: ModalHeading "アカウントの削除" -->
+  <div class="docdog-modal__body__section">
+      <p class="docdog-modal__body__text">アカウントを削除すると○○ができなくなります。アカウントを削除しますか？</p>
+    </div>
+    <div class="docdog-modal__body__section">
+    <form>
+      <div class="docdog-form__button">
+        <button type="submit" class="docdog-button docdog-button--danger">
+          アカウントを削除する
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+  methods: {
+  },
+};
+</script>
