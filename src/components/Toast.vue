d5eaaba55e408cb30114e413c8061c2122fd49da
mimori <mimori@diverta.co.jp>
2022-03-09 19:42:29 +0900

    modify: add contract / expand toast button

--- src/components/Toast.vue
+++ src/components/Toast.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="docdog">
-    <section class="docdog-toast">
+    <button type="button" class="docdog-toast--contract" v-if="!toast_expand" @click="toast_expand = !toast_expand">
+      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M7.41 16L12 11.42L16.59 16L18 14.59L12 8.59L6 14.59L7.41 16Z" fill="#ffffff"/>
+      </svg>
+    </button>
+    <section class="docdog-toast" v-if="toast_expand">
       <header class="docdog-toast__head">
         <p class="docdog-toast__head__heading">ダウンロードリスト</p>
-        <button type="button" aria-label="Close" class="docdog-toast__head__close" @click="removeAll">
-          <svg width="14" height="15" viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M11.0827 4.239L10.2602 3.4165L6.99935 6.67734L3.73852 3.4165L2.91602 4.239L6.17685 7.49984L2.91602 10.7607L3.73852 11.5832L6.99935 8.32234L10.2602 11.5832L11.0827 10.7607L7.82185 7.49984L11.0827 4.239Z"
-              fill="#AAAAAA"
-            />
+        <button type="button" aria-label="Close" class="docdog-toast__head__close" @click="toast_expand = !toast_expand">
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M16.59 8.58997L12 13.17L7.41 8.58997L6 9.99997L12 16L18 9.99997L16.59 8.58997Z" fill="#666666"/>
           </svg>
         </button>
       </header>
-
       <!-- ダウンロードリストモーダル -->
       <div class="docdog-toast__body">
         <ul class="docdog-toast__body__list">
@@ -45,7 +46,9 @@ export default {
     },
   },
   data() {
-    return {};
+    return {
+      toast_expand: true,
+    };
   },
   methods: {
     downloadAll() {
@@ -53,9 +56,6 @@ export default {
     },
     removeByIdx(idx) {
       this.$emit('removeToast', idx);
-    },
-    removeAll() {
-      this.$emit('removeToast', null); // Removes all
     },
   },
 };
