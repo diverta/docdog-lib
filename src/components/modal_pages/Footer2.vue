251c819a3234ff371c48c426a1b89f96a9ef0c77
mimori <mimori@diverta.co.jp>
2022-03-09 19:46:19 +0900

    modify: download list page

--- src/components/modal_pages/Footer2.vue
+++ src/components/modal_pages/Footer2.vue
@@ -1,12 +1,22 @@
 <template>
+
+  <!-- TODO: Show only list count > 0 -->
   <div class="docdog-button__list">
-    <button type="button" class="docdog-button docdog-button--white"  @click.prevent="redirect({ target: 'List' })">
+    <button type="button" class="docdog-button docdog-button--white" @click.prevent="redirect({ target: 'List' })">
       資料一覧に戻る
     </button>
     <button type="button" class="docdog-button docdog-button--primary" @click="downloadToast">
       まとめてダウンロードする
     </button>
   </div>
+
+  <!-- TODO: Show only list count == 0 -->
+  <div class="docdog-button__list">
+    <button type="button" class="docdog-button docdog-button--white docdog-button--wide" @click.prevent="redirect({ target: 'List' })">
+      資料一覧に戻る
+    </button>
+  </div>
+
 </template>
 
 <script>
