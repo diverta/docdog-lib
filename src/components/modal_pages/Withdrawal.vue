1e9fd949c6ee8dee652e954a9da0bc64db667653
mimori <mimori@diverta.co.jp>
2022-03-18 16:49:35 +0900

    fix: form page background-style

--- src/components/modal_pages/Withdrawal.vue
+++ src/components/modal_pages/Withdrawal.vue
@@ -1,33 +1,32 @@
 <template>
-  <div class="docdog-form">
+  <div class="docdog-container--form">
     <AlertSuccess v-if="msg" :msg="msg" :msg2="msg2" />
-
-    <div class="docdog-modal__body__section" v-if="isLogin">
-      <h1 class="docdog-modal__body__pagetitle">アカウントの削除</h1>
-      <p class="docdog-modal__body__text">
-        アカウントを削除すると今後は資料ダウンロードができなくなります。アカウントを削除しますか？
-      </p>
-    </div>
-
-    <div class="docdog-modal__body__section" v-if="isLogin">
-      <form>
-        <div class="docdog-form__button">
-          <button type="submit" class="docdog-button docdog-button--danger" @click.prevent="withdrawal">
-            アカウントを削除する
-          </button>
-        </div>
-        <div class="docdog-form__link">
-          <button type="button" class="docdog-button--text" @click.prevent="redirect({ target: 'List' })">
-            資料一覧へ戻る
-          </button>
-        </div>
-      </form>
-    </div>
-
-    <div class="docdog-modal__body__section" v-if="!isLogin">
-      <button type="button" class="docdog-button docdog-button--white" @click.prevent="redirect({ target: 'List' })">
-        資料一覧へ戻る
-      </button>
+    <div class="docdog-container--white">
+      <div class="docdog-modal__body__section" v-if="isLogin">
+        <h1 class="docdog-modal__body__pagetitle">アカウントの削除</h1>
+        <p class="docdog-modal__body__text">
+          アカウントを削除すると今後は資料ダウンロードができなくなります。アカウントを削除しますか？
+        </p>
+      </div>
+      <div class="docdog-modal__body__section" v-if="isLogin">
+        <form>
+          <div class="docdog-form__button">
+            <button type="submit" class="docdog-button docdog-button--danger" @click.prevent="withdrawal">
+              アカウントを削除する
+            </button>
+          </div>
+          <div class="docdog-form__link">
+            <button type="button" class="docdog-button--text" @click.prevent="redirect({ target: 'List' })">
+              資料一覧へ戻る
+            </button>
+          </div>
+        </form>
+      </div>
+      <div class="docdog-modal__body__section" v-if="!isLogin">
+        <button type="button" class="docdog-button docdog-button--white" @click.prevent="redirect({ target: 'List' })">
+          資料一覧へ戻る
+        </button>
+      </div>
     </div>
   </div>
 </template>
