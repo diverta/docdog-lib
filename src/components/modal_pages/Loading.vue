c9f91b2119861c69b2e4b74709d9e55e82f2b2c4
mimori <mimori@diverta.co.jp>
2022-03-23 17:09:41 +0900

    fix: Add loader message

--- src/components/modal_pages/Loading.vue
+++ src/components/modal_pages/Loading.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="docdog-loader">Loading...</div>
+  <div class="docdog-loader">
+    <div class="docdog-loader__message">圧縮ファイル作成中</div>
+    <div class="docdog-loader__icon">Loading...</div>
+  </div>
 </template>
 
 <script>
