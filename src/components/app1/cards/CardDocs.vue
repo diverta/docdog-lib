f80cd6814424c07730ce6b72e666538f4bd53d48
mimori <mimori@diverta.co.jp>
2022-06-08 19:44:37 +0900

    Fix card button click event

--- src/components/app1/cards/CardDocs.vue
+++ src/components/app1/cards/CardDocs.vue
@@ -1,21 +1,20 @@
 <template>
-  <div class="docdog-card docdog-card--link">
+  <div class="docdog-card docdog-card--link" @click.prevent="redirect({ target: 'Download', params: { doc_data: data } })">
     <div
-      class="docdog-card__thumb docdog-card--link"
+      class="docdog-card__thumb"
       :style="thumbnailStyle"
-      @click.prevent="redirect({ target: 'Download', params: { doc_data: data } })"
     >
       <span class="docdog-card__thumb__badge">{{ data.type ? data.type.label : '' }}</span>
     </div>
-    <div class="docdog-card__body" @click.prevent.self="redirect({ target: 'Download', params: { doc_data: data } })">
+    <div class="docdog-card__body">
       <p class="docdog-card__body__title">{{ data.subject }}</p>
     </div>
     <template v-if="showDownloadBtn">
-      <div class="docdog-card__foot docdog-u-d-flex-justify-center" @click.prevent.self="redirect({ target: 'Download', params: { doc_data: data } })">
+      <div class="docdog-card__foot docdog-u-d-flex-justify-center">
         <button type="button" class="docdog-button--add-list" v-if="isInToast" disabled>
           追加済み
         </button>
-        <button type="button" class="docdog-button--add-list" v-else @click="onAdd()">
+        <button type="button" class="docdog-button--add-list" v-else @click.stop="onAdd()">
           ダウンロードリストに追加
         </button>
       </div>
