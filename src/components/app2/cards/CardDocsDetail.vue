047b14ff6d6cfbc1f719d94ff08785ccd6a285b6
mimori <mimori@diverta.co.jp>
2022-07-14 21:13:41 +0900

    Modify: Separate display of buttons for pc and sp on docs detail page

--- src/components/app2/cards/CardDocsDetail.vue
+++ src/components/app2/cards/CardDocsDetail.vue
@@ -2,13 +2,15 @@
   <div class="docdog-article">
     <div class="docdog-article__title">
       <h2>{{ data.subject }}</h2>
-      <AddToastButton @click.stop="onAdd()" :isInToast="isInToast" />
+      <AddToastButton @click.stop="onAdd()" :isInToast="isInToast" class="docdog-u-hidden-sp" />
     </div>
     <figure v-if="data.thumbnail && data.thumbnail.url" class="docdog-u-my-lg">
       <img :src="data.thumbnail.url" />
     </figure>
     <div v-if="data.wysiwyg" v-html="data.wysiwyg" class="docdog-u-my-lg"></div>
-    <AddToastButton @click.stop="onAdd()" :isInToast="isInToast" />
+    <div class="docdog-button--fixed--sp">
+      <AddToastButton @click.stop="onAdd()" :isInToast="isInToast" />
+    </div>
   </div>
 </template>
 
