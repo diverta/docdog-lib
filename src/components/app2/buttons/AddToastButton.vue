ad757b5f3eb4841b58e518d3c66927785e6fc0ac
mimori <mimori@diverta.co.jp>
2022-07-14 21:11:17 +0900

    Fix: button styles

--- src/components/app2/buttons/AddToastButton.vue
+++ src/components/app2/buttons/AddToastButton.vue
@@ -1,5 +1,6 @@
 <template>
-  <button type="button" class="docdog-button--add-list" :disabled="isInToast">
+  <!-- TODO: Change to toggle -->
+  <button type="button" class="docdog-button--add-list" :class="isInToast ? 'docdog-button--add-list--disabled' : ''">
     <span v-if="isInToast">追加済み</span>
     <span v-else>ダウンロードリストに追加</span>
   </button>
